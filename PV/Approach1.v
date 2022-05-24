1e1b99a1ce8a48a8590fd4bb7ce6c0610921a0e9
My <my.nordaljensen@gmail.com>
2022-05-24 23:30:08 +0200

    Main approaches copied to files. Rough edit

--- PV/Approach1.v
+++ PV/Approach1.v
@@ -10,7 +10,8 @@ From Coq Require Import Strings.String.
 (* From PV Require Import Maps. *)
 
 From mathcomp.ssreflect Require Import all_ssreflect.
-From infotheo Require Import fsdist.
+From mathcomp Require Import finmap.
+From infotheo Require Import fsdist proba.
 Require Import Reals.
 
 Definition var := ordinal 64.
@@ -100,53 +101,12 @@ Notation "x '!->' v ';' m" := (t_update m x v)
   (at level 100, v at next level, right associativity).
 
 Lemma t_apply_empty : forall x v,
-  (_ !-> v) x = v.
-  Proof.
-    move => x v.
-    rewrite /t_empty=> /=.
-    by rewrite ffunE.
-  Qed.
-
-Lemma t_update_eq : forall m x v,
-  (x !-> v ; m) x = v.
+(_ !-> v) x = v.
 Proof.
-  move => m x v.
-  rewrite /t_update => /=.
-  by rewrite ffunE ifT.
-  Qed.
-
-
-Theorem t_update_neq : forall (m : state) x1 x2 v,
-  x1 <> x2 -> (x1 !-> v ; m) x2 = m x2.
-Proof.
-  move => m x1 x2 v.
-  rewrite /t_update => /=.
-  intros h1.
-  rewrite ffunE ifT.
-  destruct h1.
-  Admitted.
-
-
-Lemma t_update_shadow : forall m x v1 v2,
-  (x !-> v2 ; x !-> v1 ; m) = (x !-> v2 ; m).
-  Proof.
-    move => m x v1 v2.
-    rewrite /t_update.
-    apply ffunP => x0.
-    case (eq_dec x x0) => eq.
-    rewrite !ffunE !ifT => //; admit.
-    rewrite !ffunE !ifT => //; admit.
-Admitted.
-
-
-Theorem t_update_same : forall (m : state) x,
-  (x !-> m x ; m) = m.
-Proof.
-    move => m x.
-    rewrite /t_update => /=.
-    rewrite -(ffunE m).
-Admitted.
-
+move => x v.
+rewrite /t_empty=> /=.
+by rewrite ffunE.
+Qed.
 
 Inductive com : Type :=
   | CSkip
@@ -182,24 +142,7 @@ Notation "x '$=' { a1 ; a2 ; .. ; an }" := (sample x a1 (cons a2 .. (cons an nil
   (in custom com at level 0, x constr at level 0,
   a1 at level 85, a2 at level 85, an at level 85, no associativity) : com_scope.
 
-
-Definition plus2 : com :=
-  <{ X := X + 2 }>.
-
 Definition half : Reals_ext.Prob.t := Reals_ext.probdivRnnm 1 2.
-
-Definition split : com :=
-  <{ skip [+ half] skip }>.
-
-Definition sampleex : com :=
-  <{ X $= {X; 0 + 0} }>.
-
-Definition XtimesYinZ : com :=
-  <{ Z := X * Y }>.
-
-Definition subtract_slowly_body : com :=
-  <{ Z := Z - 1 ;
-      X := X - 1 }>.
 
 
 Fixpoint ceval (st : state) (c : com) :=
@@ -216,6 +159,123 @@ Fixpoint ceval (st : state) (c : com) :=
   | <{ c1 [+ p] c2}> => ConvFSDist.d p (ceval st c1) (ceval st c2)
   end.
 
+
+
+
+
+Definition Assertion := state -> Prop.
+
+Definition assert_implies (P Q : Assertion) : Prop :=
+    forall st, P st -> Q st.
+
+
+Declare Scope hoare_spec_scope.
+Notation "P ->> Q" := (assert_implies P Q)
+                        (at level 80) : hoare_spec_scope.
+Open Scope hoare_spec_scope.
+
+Notation "P <<->> Q" :=
+(P ->> Q /\ Q ->> P) (at level 80) : hoare_spec_scope.
+
+Definition Aexp : Type := state -> nat.
+
+Definition assert_of_Prop (P : Prop) : Assertion := fun _ => P.
+Definition Aexp_of_nat (n : nat) : Aexp := fun _ => n.
+Definition Aexp_of_aexp (a : aexp) : Aexp := fun st => aeval st a.
+
+
+Coercion assert_of_Prop : Sortclass >-> Assertion.
+Coercion Aexp_of_nat : nat >-> Aexp.
+Coercion Aexp_of_aexp : aexp >-> Aexp.
+
+Arguments assert_of_Prop /.
+Arguments Aexp_of_nat /.
+Arguments Aexp_of_aexp /.
+
+Declare Scope assertion_scope.
+Bind Scope assertion_scope with Assertion.
+Bind Scope assertion_scope with Aexp.
+Delimit Scope assertion_scope with assertion.
+
+Notation assert P := (P%assertion : Assertion).
+Notation mkAexp a := (a%assertion : Aexp).
+Notation "~ P" := (fun st => ~ assert P st) : assertion_scope.
+Notation "P /\ Q" := (fun st => assert P st /\ assert Q st) : assertion_scope.
+Notation "P \/ Q" := (fun st => assert P st \/ assert Q st) : assertion_scope.
+Notation "P -> Q" := (fun st => assert P st ->  assert Q st) : assertion_scope.
+Notation "P <-> Q" := (fun st => assert P st <->  assert Q st) : assertion_scope.
+Notation "a = b" := (fun st => mkAexp a st = mkAexp b st) : assertion_scope.
+Notation "a <> b" := (fun st => mkAexp a st <> mkAexp b st) : assertion_scope.
+Notation "a <= b" := (fun st => mkAexp a st <= mkAexp b st) : assertion_scope.
+Notation "a < b" := (fun st => mkAexp a st < mkAexp b st) : assertion_scope.
+Notation "a >= b" := (fun st => mkAexp a st >= mkAexp b st) : assertion_scope.
+Notation "a > b" := (fun st => mkAexp a st > mkAexp b st) : assertion_scope.
+Notation "a + b" := (fun st => mkAexp a st + mkAexp b st) : assertion_scope.
+Notation "a - b" := (fun st => mkAexp a st - mkAexp b st) : assertion_scope.
+Notation "a * b" := (fun st => mkAexp a st * mkAexp b st) : assertion_scope.
+
+
+(* ################## HOARE QUADRUPLES ################# *)
+
+Local Open Scope proba_scope.
+Local Open Scope com_scope.
+
+Definition hoare_quad
+            (P : Assertion) (c : com) (Q : Assertion) (d : R) : Prop :=
+    forall st,
+        P st ->
+        let dst' := (ceval st c) in
+        forall (s : {set [finType of finsupp dst']}),
+        (forall st', st' \in s <-> Q (fsval st')) ->
+        Pr (fdist_of_Dist dst') s = d.
+
+Theorem test_skip: forall P,
+hoare_quad P CSkip P 1.
+Proof.
+move => P /=.
+rewrite /hoare_quad.
+    move => st HPst s HQs //.
+    rewrite /FSDist1.d /FSDist1.f.
+    apply HPst in HQs. Admitted.
+
+
+Theorem test_seq : forall P Q R d1 d2 c1 c2,
+    hoare_quad Q c2 R d1 ->
+    hoare_quad P c1 Q d2 ->
+    hoare_quad P (CSeq c1 c2) R (d1 * d2).
+Proof. Admitted.
+
+
+
+Definition bassn b : Assertion :=
+  fun st => (beval st b = true).
+
+Coercion bassn : bexp >-> Assertion.
+
+Arguments bassn /.
+
+
+Theorem test_if : forall P Q (b: bexp) d c1 c2,
+  hoare_quad (P /\ bassn b) c1 Q d ->
+  hoare_quad (P /\ ~b) c2 Q d ->
+  hoare_quad P (CIf b c1 c2) Q d.
+Proof. Admitted.
+
+
+(*Theorem test_plus : forall P Q c1 c2 p,
+  hoare_quad P c1 Q p ->
+  hoare_quad P c2 Q (1-p) ->
+  hoare_quad P (CPlus p c1 c2) Q.*)
+
+
+Lemma twoCoins : forall x y,
+hoare_quad
+    BTrue
+    (CSeq (CPlus half (CAsgn x (ANum 1)) (CAsgn x (ANum 2))) (CPlus half (CAsgn y (ANum 1)) (CAsgn y (ANum 2))))
+    (x + y = 3)
+    (Reals_ext.Prob.p half).
+Proof.
+intros. unfold hoare_quad. intros. destruct x. Admitted.
 
 
 
